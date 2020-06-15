4bfb03530819eaee5c9838b94b89aa6a7f69d598
fabulous <dnf126@gmail.com>
2020-06-16 04:13:56 +0900

    v1.0.4

--- mybasic/src/main/java/tkcore/info/mybasic/FunBasic.kt
+++ mybasic/src/main/java/tkcore/info/mybasic/FunBasic.kt
@@ -6,7 +6,7 @@ import android.util.Log
 /*************************************************************************************/
 
 private val tag = "tttest"
-  val test1 = "test111"
+  val test1 = "test222"
 fun lllogI(str: String?) {
     Log.i(tag, str?:"none")
 }
