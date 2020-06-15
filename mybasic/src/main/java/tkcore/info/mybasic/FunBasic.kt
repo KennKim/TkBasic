8fba91df5062501b6f692d025239ed6307cd27b7
fabulous <dnf126@gmail.com>
2020-06-16 03:32:39 +0900

    Initial commit

---
+++ mybasic/src/main/java/tkcore/info/mybasic/FunBasic.kt
@@ -0,0 +1,62 @@
+package tkcore.info.mybasic
+import android.util.Log
+
+/*************************************************************************************/
+/********************  ***********************************************************/
+/*************************************************************************************/
+
+private val tag = "tttest"
+fun lllogI(str: String?) {
+    Log.i(tag, str?:"none")
+}
+fun lllogItoast(str: String?) {
+    lllogI(str)
+//    toastNormal(str)
+}
+
+fun lllogW(str: String?) {
+    Log.w(tag, str?:"none")
+}
+
+fun lllogW(e: Exception) {
+    Log.w(tag, e)
+}
+
+fun lllogW(str: String?, e: Exception) {
+    Log.w(tag, str, e)
+}
+
+fun lllogD(str: String?) {
+    Log.d(tag, str?:"none")
+}
+
+fun lllogD(str: String?, e: Exception) {
+    Log.d(tag, str, e)
+}
+
+fun lllogE(str: String?) {
+    Log.e(tag, str?:"none")
+}
+
+
+
+//
+///**
+// * Toast 날리고나서 true도 날려줄 때.
+// */
+//fun trueToast(msg: String): Boolean {
+//    TToast.showAlert(msg)
+//    return true
+//}
+//
+//fun trueToast(strId: Int): Boolean {
+//    TToast.showAlert(strId)
+//    return true
+//}
+//
+///**
+// * when문 else로 빠졌을 때.
+// */
+//fun errorWhen() {
+//    TToast.showNormal(R.string.error_when_else)
+//}
